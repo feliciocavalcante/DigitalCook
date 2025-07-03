a94b6509d2703620964f202a3d80c4388cff1bb2
feliciocavalcante <feliciospfc@gmail.com>
2025-07-03 10:55:04 -0300

    DigitalCook

---
+++ DigitalCook/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text text-4xl p-2 text-center font-bold " >Digital Cook</h1>
+      <p className="text text-2xl p-2 text-center">Felicio Cavalcante</p>
+    </>
+  )
+}
+
+export default App
