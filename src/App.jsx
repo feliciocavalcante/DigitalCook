f78ac471f6cc3c8c6d1ad9ea5f3cc4bc4974460b
GutoAtanasio <guto.allm@gmail.com>
2025-07-03 13:09:59 -0300

    guto

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
     <>
       <h1 className="text text-4xl p-2 text-center font-bold " >Digital Cook</h1>
       <p className="text text-2xl p-2 text-center text-red-700">Felicio Cavalcante</p>
+      <p className="text text-2xl p-2 text-center text-red-700">Guto Almeida</p>
     </>
   )
 }
