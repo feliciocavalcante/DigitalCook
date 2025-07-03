02caeb1b8a10cddf7eb23cd5af8cbfaf359eeb55
feliciocavalcante <feliciospfc@gmail.com>
2025-07-03 11:04:43 -0300

    att

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <h1 className="text text-4xl p-2 text-center font-bold " >Digital Cook</h1>
-      <p className="text text-2xl p-2 text-center text-red-600">Felicio Cavalcante</p>
+      <p className="text text-2xl p-2 text-center text-red-700">Felicio Cavalcante</p>
     </>
   )
 }
