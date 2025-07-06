3f91786e9e4bf9c42db193a95445a1dfa7c33c1c
Isaac Lima <isaaclima64178@gmail.com>
2025-07-06 20:45:26 -0300

    atualizaçao do header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,61 @@
+import { CookingPot, Heart, MagnifyingGlass, Star, User } from 'phosphor-react'
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      <h1 className="text text-4xl p-2 text-center font-bold " >Digital Cook</h1>
-      <p className="text text-2xl p-2 text-center text-red-700">Felicio Cavalcante</p>
-      <p className="text text-2xl p-2 text-center text-red-700">Guto Almeida</p>
-      <p className="text text-2xl p-2 text-center text-red-700">Isaac lima</p>
-    </>
+    <header className='flex flex-col w-full h-screen'>
+      <div className="flex w-full h-12 bg-black justify-between p-4 items-center">
+
+        <div className="flex gap-2 justify-center items-center">
+          <CookingPot size={33} color="#ffffff" weight="bold" className='bg-amber-400 p-1 rounded-md' />
+          <div className="flex flex-col">
+            <h1 className="text-amber-400 text-xl font-medium">DigitalCook</h1>
+            <p className="text-[12px] text-gray-400">App de Receitas</p>
+          </div>
+        </div>
+
+n
+        <div className="flex gap-3 items-center">
+
+          <MagnifyingGlass size={20} color="#ffffff" weight="bold" />
+          <input className='border-1 border-white justify-center text text-center text-white w-100 h-7 rounded-2xl' type="search" name="" id="" />
+
+        </div>
+
+        <div className="flex gap-8 p-2">
+          <Heart size={22} color="#ffffff" weight="bold" />
+          <User size={22} color="#ffffff" weight="bold" />
+        </div>
+
+      </div>
+      <div className=" flex-col w-full h-full bg-[url('https://blog.consumer.com.br/wp-content/uploads/2020/11/culin%C3%A1ria-regional-brasileira.jpg')] bg-cover justify-center items-center flex gap-5 ">
+
+        <div className="flex items-center justify-center bg-amber-400 w-40 h-6 gap-1 rounded-2xl">
+          <Star size={13} color="#b1023f" weight="bold" />
+          <p className='text-[13px] text-center text-white'>Mais de 1000 receitas</p>
+        </div>
+
+        <div className="">
+          <h1 className="text-5xl text-white  ml-10">
+            Descubra o
+          </h1>
+
+          <h1 className="text-5xl text-amber-500 font-bold">
+            Sabor Perfeito
+          </h1>
+
+        </div>
+
+        <div className="">
+          <p className='text-white break-keep font-medium bg-amber-600 p-1 rounded-md'>
+            Explore receitas incriveis, compartinhe seus pratos favoritos e transforme sua cozinha em uma verdadeira obra de arte culinária.
+          </p>
+        </div>
+
+
+      </div>
+    </header>
   )
 }
 
