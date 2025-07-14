d438e0037cb70cd9f6f5b9bafeb22d524aaa2105
Isaac Lima <isaaclima64178@gmail.com>
2025-07-13 21:40:08 -0300

    att isaac

--- src/pages/Receita2.jsx
+++ src/pages/Receita2.jsx
@@ -9,33 +9,34 @@ function Receita2() {
     }
 
     return (
-        <div>
-            <div>
-                <img src={macarao} alt="Macarrão com molho cremoso de cogumelos"/>
+        <div className='flex w-full h-screen bg-black'>
+            <div className=' flex justify-center items-center ml-10 '>
+                <img className='flex rounded-xl ' src={macarao} alt="Macarrão com molho cremoso de cogumelos" />
 
-                <div >
-                    <h1 >Receita </h1>
-                    <h2 >Macarrão com molho cremoso de cogumelos</h2>
+            </div>
+            <div className='w-1/2 flex flex-col justify-center items-center gap-3 border-2 border-amber-400 rounded-xl m-10 '>
+            
+                <h1 className='text-amber-400 text-[40px] font-medium h-12 ' >Receita </h1>
+                <h2 className='text-amber-400 text-[25px] font-medium ' >Macarrão com molho cremoso  de cogumelos</h2>
 
-                    <p >Ingredientes:</p>
-                    <div >
-                        <p>
-                            1 colher de sopa de manteiga<br />
-                            250g de cogumelos fatiados<br />
-                            3 dentes de alho<br />
-                            Tomilho (opcional)<br />
-                            1/4 xícara de vinho branco<br />
-                            1 e 1/2 xícara de creme de leite fresco<br />
-                            Sal e pimenta do reino a gosto<br />
-                            Parmesão a gosto<br />
-                            200g de massa de sua preferência
-                        </p>
-                    </div>
-
-                    <button onClick={handleHome} >
-                        Voltar para Home
-                    </button>
+                <p className='text-white text-[18px] font-medium ' >Ingredientes:</p>
+                <div className='text-white text-[15px] font-medium ml-10 ' >
+                    <p>
+                        1 colher de sopa de manteiga<br />
+                        250g de cogumelos fatiados<br />
+                        3 dentes de alho<br />
+                        Tomilho (opcional)<br />
+                        1/4 xícara de vinho branco<br />
+                        1 e 1/2 xícara de creme de leite fresco<br />
+                        Sal e pimenta do reino a gosto<br />
+                        Parmesão a gosto<br />
+                        200g de massa de sua preferência
+                    </p>
                 </div>
+
+                <button className='bg-amber-400 text-black font-bold py-2 px-4 rounded mt-7 ml-3' onClick={handleHome} >
+                    Voltar para Home
+                </button>
             </div>
         </div>
     );
--- src/pages/Receita3.jsx
+++ src/pages/Receita3.jsx
@@ -9,37 +9,38 @@ function Receita3() {
     }
 
     return (
-        <div >
-            <div>
-                <img src={salada} alt="Salada Mediterranean"/>
+        <div className='flex w-full h-screen bg-black overflow-hidden' >
+            <div className=' flex justify-center items-center ml-2 '>
+                <img className='flex rounded-xl ' src={salada} alt="Salada Mediterranean" />
 
-                <div>
-                    <h1 >Salada Mediterranean</h1>
+            </div>
+            <div className='w-1/2 flex flex-col justify-center gap-2 items-center border-2 border-amber-400 rounded-xl m-3 p-3'>
+                <h1 className='text-amber-400 text-[40px] font-medium h-12  ' >Receita </h1>
+                <h2 className='text-amber-400 text-[25px] font-medium ' >Salada Mediterranean</h2>
 
-                    <p >Ingredientes:</p>
+                <p  className='text-white text-[18px] font-medium ' >Ingredientes:</p>
 
-                    <div >
-                        <p>
-                           Grão-de-bico: O feijão enlatado é um verdadeiro salva-vidas quando se trata de um alimento básico favorito na despensa. Cada lata padrão de 425g de grão-de-bico vem repleta de fibras e proteínas, fornecendo uma dose dupla de nutrição real e acessível nos 30 segundos necessários para abrir uma lata, escorrer e enxaguar. Eles podem ser consumidos direto da lata, tornando esta uma receita muito adequada para o verão naqueles dias muito quentes, quando você simplesmente não consegue ligar o fogo. Eu sei que esta é uma salada de "grão-de-bico", mas você pode trocar o grão-de-bico pelo seu feijão enlatado favorito: feijão-branco cremoso, feijão-preto e feijão-carioca são ótimas opções.
-                        </p>
+                <div className='text-white text-[11px] font-medium ' >
+                    <p className="flex flex-col text-center mb-1">
+                        <span className="flex justify-center font-bold text-[16px]">Grão-de-bico:</span> O feijão enlatado é um verdadeiro salva-vidas quando se trata de um alimento básico favorito na despensa. Cada lata padrão de 425g de grão-de-bico vem repleta de fibras e proteínas, fornecendo uma dose dupla de nutrição real e acessível nos 30 segundos necessários para abrir uma lata, escorrer e enxaguar. Eles podem ser consumidos direto da lata, tornando esta uma receita muito adequada para o verão naqueles dias muito quentes, quando você simplesmente não consegue ligar o fogo. Eu sei que esta é uma salada de "grão-de-bico", mas você pode trocar o grão-de-bico pelo seu feijão enlatado favorito: feijão-branco cremoso, feijão-preto e feijão-carioca são ótimas opções.
+                    </p>
 
-                        <p >
-                            Vegetais: Pepino picado, pimentão vermelho, cebola roxa e azeitonas Kalamata são meus ingredientes favoritos para esta salada. Embora eu adore esta salada como ela é, se você quiser substituir algum dos ingredientes da salada pelo seu acompanhamento preferido, temos a sua bênção. Não gosta de azeitonas ou pimentões? Tomates-cereja ou abacate maduro em cubos preencherão essa lacuna de sabor.
-                        </p>
+                    <p className="flex flex-col text-center mb-1" >
+                        <span className="flex justify-center font-bold text-[16px]">Vegetais:</span> Pepino picado, pimentão vermelho, cebola roxa e azeitonas Kalamata são meus ingredientes favoritos para esta salada. Embora eu adore esta salada como ela é, se você quiser substituir algum dos ingredientes da salada pelo seu acompanhamento preferido, temos a sua bênção. Não gosta de azeitonas ou pimentões? Tomates-cereja ou abacate maduro em cubos preencherão essa lacuna de sabor.
+                    </p>
 
-                        <p >
-                            Queijo feta: Quer manter a dieta sem laticínios? Substitua o queijo feta por outra forma de proteína, como alguns ovos cozidos ou uma lata de atum, ou um punhado de nozes torradas e picadas.
-                        </p>
+                    <p className="flex flex-col text-center mb-1" >
+                        <span className="flex justify-center font-bold text-[16px]">Queijo feta:</span> Quer manter a dieta sem laticínios? Substitua o queijo feta por outra forma de proteína, como alguns ovos cozidos ou uma lata de atum, ou um punhado de nozes torradas e picadas.
+                    </p>
 
-                        <p >
-                        O Molho: Eu criei um molho simples para acompanhar toda esta salada com azeite extravirgem, vinagre de vinho branco, suco de limão fresco, salsa fresca picada e flocos de pimenta vermelha. Adoro a simplicidade e o equilíbrio de sabores, mas há algumas coisas a serem observadas aqui. Recomendo fortemente o uso de suco de limão fresco para obter o melhor sabor possível; se você não gosta de pimenta, pode reduzir os flocos de pimenta vermelha ou ajustar a gosto. Use seu azeite extravirgem favorito ou confira nosso ranking das melhores marcas de azeite.
-                        </p>
-                    </div>
-
-                    <button onClick={handleHome}>
-                        Voltar para Home
-                    </button>
+                    <p className="flex flex-col text-center" >
+                        <span className="flex justify-center font-bold text-[16px]">O Molho:</span> Eu criei um molho simples para acompanhar toda esta salada com azeite extravirgem, vinagre de vinho branco, suco de limão fresco, salsa fresca picada e flocos de pimenta vermelha. Adoro a simplicidade e o equilíbrio de sabores, mas há algumas coisas a serem observadas aqui. Recomendo fortemente o uso de suco de limão fresco para obter o melhor sabor possível; se você não gosta de pimenta, pode reduzir os flocos de pimenta vermelha ou ajustar a gosto. Use seu azeite extravirgem favorito ou confira nosso ranking das melhores marcas de azeite.
+                    </p>
                 </div>
+
+                <button className='bg-amber-400 hover:bg-amber-500 text-black font-bold py-2 px-4 rounded mt-3' onClick={handleHome}>
+                    Voltar para Home
+                </button>
             </div>
         </div>
     );
